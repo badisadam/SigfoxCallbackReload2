65eda6dbeb7ea6c70df79ca06743c413395feb9d
badis kherraza <badis.kherraza@greencityzen.fr>
2017-05-31 10:06:11 +0200

    Delete serveur_et_retour.js

--- serveur_et_retour.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-app.get('/etage/:etagenum/chambre', function (req, res) {
-    res.render('chambre.ejs', { etage: req.params.etagenum });
-});
-
-app.listen(8080);
