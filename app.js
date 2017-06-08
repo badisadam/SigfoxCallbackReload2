dc2ce7c27790104177a3292e8dff8402d1144bd5
badisadam <badis.kherraza@greencityzen.fr>
2017-06-08 18:09:25 +0200

    maj 5.1

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var http = require('http');
 var session = require('cookie-session'); // Charge le middleware de sessions
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var dataresp1;
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -17,48 +16,34 @@ var app = express();
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-/*var numdevicetest = '7A501';
-var phrase = 'https://backend.sigfox.com/api/devices/'+numdevicetest+'/messages'
- axios.get('https://backend.sigfox.com/api/devices/'+numdevicetest+'/messages', {
-    auth: {
-        username: '5937bc6f9e93a13f76ef0764',
-        password: '49bd406abd0da432b87cc7a9e9efe4df'
-    },
-    responseType: 'json'
-}).then(function (res) {
-
-    dataresp2 = res.data;
-     dataresp1 = JSON.stringify(dataresp2);
-    console.log(res.data);
-
-
-
-});
+var numdevice;
+var reponse_json ;
+/*writeHead(200, { 'Content-Type': 'text/html' });
+res.write('test1' + dataresp1);
+utilisé pour afficher de l'html
 */
-//JSON.stringify(
-
-
-
-
-app.get('/reponse', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('test1' + dataresp1);
-});
-
 
 
 
 /* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+app.use(session({secret :'badissecret'}))
 
 /* S'il n'y a pas de todolist dans la session,
  on en crée une vide sous forme d'array avant la suite */
     .use(function(req, res, next){
-        if (typeof(req.session.tableaucallback) == 'undefined') {
+        if (typeof(req.session.tableaucallback) === 'undefined') {
             req.session.tableaucallback = [];
         }
         next();
     })
+
+
+
+
+
+
+
+
 
     /* On affiche la todolist et le formulaire */
     .get('/asking', function(req, res) {
@@ -68,35 +53,45 @@ app.use(session({secret: 'todotopsecret'}))
     .post('/asking/ajouter/', urlencodedParser, function(req, res) {
 
         if (req.body.deviceid != '') {
-            var retourget;
-            var numdevice = req.body.deviceid;
 
-            req.session.tableaucallback.push(numdevice);
-            //test implementation de requete get
-            axios.get('https://backend.sigfox.com/api/devices/' + numdevice + '/messages', {
-                auth: {
-                    username: '5937bc6f9e93a13f76ef0764',
-                    password: '49bd406abd0da432b87cc7a9e9efe4df'
-                },
-                responseType: 'json'
-            })
+            numdevice = req.body.deviceid; //recuperation de la valeur du device id
+
+            req.session.tableaucallback.push(numdevice); // push du device id dans le array tableaucallback
+            //envoi de la requete GET et récupération dans une variable
+           console.log(axios.get('https://backend.sigfox.com/api/devices/' + numdevice + '/messages?limit=1', {
+                    auth: {
+                        username: '5937bc6f9e93a13f76ef0764',
+                        password: '49bd406abd0da432b87cc7a9e9efe4df'
+                    },
+                    responseType: 'json',
+
+                }
+
+            )).then(function(res){
+
+           })
+           // JSON.stringify(//
+                //.then(function (res){
+
+
+                  // reponse_json = JSON.stringify(console.log(res.data));
+
+                //})
+
         }
 
-        console.log(res.data);
-        var a = res.data;
-        var b = JSON.stringify(a);
-        retourget = b;
-        for (var i = 0; i < b.length; i++) {
 
-            req.session.tableaucallback.push(b);
+            res.redirect('/asking')
+        }     )
 
 
-        }
-        ;
 
 
-        res.redirect('/asking');
-    })
+
+
+
+
+
     /* Supprime un élément de la todolist */
     .get('/asking/supprimer/:id', function(req, res) {
         if (req.params.id != '') {
@@ -109,9 +104,9 @@ app.use(session({secret: 'todotopsecret'}))
     .use(function(req, res, next){
         res.redirect('/asking');
     });
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-
 
     // print a message when the server starts listening
     console.log("server starting on " + appEnv.url);
